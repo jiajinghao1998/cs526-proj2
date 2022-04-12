3d61bd9f65aaca3cf0674bd5cd877cbacaa75716
Jinghao Jia <jinghao7@illinois.edu>
2022-04-12 16:33:13 -0500

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# CS526 Project2
+
+### Build
+```console
+mkdir build && cd build
+cmake -GNinja -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_FLAGS=-pipe -DCMAKE_CXX_FLAGS=-pipe ..
+```
---
+++ tests/unit/trivial.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Although this is a trivial case of scalar replacement, it is
+ * instructive to study the unoptimized code.  Run "make trivial.llvm.bc",
+ * disassemble the resulting file, and study it carefully to understand the
+ * LLVM code that is generated.
+ */
+int main(int argc, char* argv[])
+{
+  int i1 = atoi(argv[1]);
+  int i2 = atoi(argv[2]);
+  
+  return i1 + i2;
+}
